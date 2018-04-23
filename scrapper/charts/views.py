a908f688e6ae045876c3189972f87d1e74d7e6e8
wonderboy <wonderboy39@outlook.com>
2018-04-23 23:37:47 +0900

    highchart 적용한 샘플 코드 커밋

--- scrapper/charts/views.py
+++ scrapper/charts/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import numpy as np
 
 
 # Create your views here.
@@ -12,11 +13,18 @@ def index(request):
 
 
 def render_chart(request):
+    time_unit = [i for i in range(1,31)]
+    data1 = np.random.randint(0, high=40, size=30).tolist
+    data2 = np.random.randint(0, high=40, size=30).tolist
+
     template_data = {
         'day': [1,2,3],
-        'temperature': [29,27,33]
+        'temperature': [29,27,33],
+        'time_unit': time_unit,
+        'data1': data1,
+        'data2': data2
     }
     if request.method == 'GET':
         print("get...")
 
-    return render(request, 'charts/chart.html',template_data)
+    return render(request, 'charts/chart.html', template_data)
